e05c9424f9de07f6ef833b42ae3ae82c15da0249
sangsil <siro9866@gmail.com>
2025-02-10 11:06:33 +0900

    배치작성

---
+++ src/main/java/com/sil/springbatch/repository/AfterRepository.java
@@ -0,0 +1,7 @@
+package com.sil.springbatch.repository;
+
+import com.sil.springbatch.entity.AfterEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AfterRepository extends JpaRepository<AfterEntity, Long> {
+}
---
+++ src/main/java/com/sil/springbatch/repository/BeforeRepository.java
@@ -0,0 +1,7 @@
+package com.sil.springbatch.repository;
+
+import com.sil.springbatch.entity.BeforeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeforeRepository extends JpaRepository<BeforeEntity, Long> {
+}
